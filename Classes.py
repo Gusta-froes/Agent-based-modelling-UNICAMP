810dd871ccd8631715a1ed463b2fc52ed52053e1
Gusta-froes <gustafroes12@gmail.com>
2021-09-02 11:20:08 -0300

    Ihave no idea what am I doing with git, but just wanted to save some changes in github

--- Classes.py
+++ Classes.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import matplotlib.animation as animation
+
+
+
 
 class Institute:
   def __init__ (self,location, area, name):
@@ -9,11 +9,13 @@ class Institute:
     self.name = name
     self.area = area
 
-class Student:
-  global dt
-
+class People:
+  asym = 0.3
   dt = 0.1
-  def __init__ (self,Inst, Infect, Position, Vaci, Velocity, Quaren, Schedule,Imune, V0,Time):
+  p_test_symp = 0.7
+  p_test_asymp = 0.1
+  
+  def __init__ (self,Inst, Infect, Position, Vaci, Velocity, Quaren, Schedule,Imune, V0,Time, Age, Incub_period, Death_period, Recov_period, Infectivity):
     self.Inst = Inst
     self.Infect = Infect
     self.Position = Position
@@ -25,11 +27,21 @@ class Student:
     self.V0 = V0
     self.Time = Time
     self.Goal = Schedule[Time[0]][Time[1]]
+    self.Age = Age
+    self.Prob_Die = 0.0000457*np.exp(0.08952*self.Age)
+    self.color =  {0:"Green",2:"Red",3:"Blue"}[Infect]
+    self.Death_period = Death_period
+    self.Incub_period = Incub_period
+    self.Recov_period = Recov_period
+    self.Infectivity = Infectivity
+    self.Is_Going_2_Die = False
+    self.timer = [8,"Mon",0]
+    
 
 
 
-  def Set_Goal(self,time):
-    self.Goal = self.Schedule[time[0]][time[1]]
+  def Set_Goal(self):
+    self.Goal = self.Schedule[self.time[0]][self.time[1]]
 
 
   def Set_P0(self, PosiInicial):
@@ -38,8 +50,76 @@ class Student:
   def Set_V0(self, v0):
     self.Velocity = v0
 
-  def att_posi(self):
+  def Att_Posi(self):
     self.Position = self.Position + self.Velocity*dt
+
+  def Att_Time (self, time):
+    self.time = time
+    self.Set_Goal()
+
+  def Att_State (self, *args):
+    if not self.Infect == -2:
+      if len(args) > 0:
+        self.timer = self.Time
+        self.Infect = args[0]
+        self.color =  {0:"Green",2:"Red",3:"Blue",1:"Green", -1:"Gray", -2:"Black"}[self.Infect]
+      else:
+        if (self.Time[2] - self.timer[2]) >= self.Death_period and self.Is_Going_2_Die == True:
+          self.Infect = -2
+          self.color =  "Black"
+
+        elif (self.Time[2] - self.timer[2]) >= self.Incub_period:
+
+          if np.random.uniform(0,1) <= self.Porb_Die:
+            self.Is_Going_2_Die = True
+    
+          if np.random.uniform(0,1) <= asym:
+            self.Infect = 3
+            self.color =  "Blue"
+            
+          else:
+            self.Infect = 2
+            self.color =  "Red"
+            self.Infectivity = np.random.gamma(1.3, 0.2)
+            while self.Infectivity > 1:
+                  self.Infectivity = np.random.gamma(1.3, 0.2)
+          self.timer = self.Time  
+          
+        elif (self.Time[2] - self.timer[2]) >= self.Recov_period:
+          self.Infect = -1
+          self.color = "Grey"
+          self.Is_Going_2_Die = False
+
+      if self.Quarentine == True and self.Infected < 0:
+        self.Quaretine = False
+      elif self.Quarentine == False and np.random.uniform(0,1) <= 0.98:
+        if self.Infect == 2 and np.random.uniform(0,1) <= p_test_symp:
+          self.Quarentine = True
+        elif self.Infect == 3 and np.random.uniform(0,1) <= p_test_asymp:
+          self.Quarentine = True
+
+  def Att_Quarentine(self):
+    if self.Infect < 0:
+      self.Quarentine = False
+
+    elif Quarentine == True:
+      self.Set_P0(np.array([100,100]))            # Still need to think where quarentine will ocour
+    
+
+
+
   
-  def att_velo(self):
-    self.Velocity = self.Velocity +a*dt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
