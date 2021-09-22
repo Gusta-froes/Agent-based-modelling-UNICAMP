5669544da11ad89409f77d880a4127708afa0186
Gusta-froes <gustafroes12@gmail.com>
2021-09-22 19:09:31 -0300

    Voltando para onde estava

--- Classes.py
+++ Classes.py
@@ -1,7 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import matplotlib.animation as animation
+
+
+
 
 class Institute:
   def __init__ (self,location, area, name):
@@ -9,11 +9,15 @@ class Institute:
     self.name = name
     self.area = area
 
-class Student:
-  global dt
-
+class People:
+  global asym
+  asym = 0.3
+  global dt, p_test_symp, p_test_asymp
   dt = 0.1
-  def __init__ (self,Inst, Infect, Position, Vaci, Velocity, Quaren, Schedule,Imune, V0,Time):
+  p_test_symp = 0.7
+  p_test_asymp = 0.1
+
+  def __init__ (self,Inst, Infect, Position, Vaci, Velocity, Quaren, Schedule,Imune,Time, Age, Incub_period, Death_period, Recov_period, Infectivity):
     self.Inst = Inst
     self.Infect = Infect
     self.Position = Position
@@ -22,24 +26,85 @@ class Student:
     self.Quaren = Quaren
     self.Schedule = Schedule
     self.Imune = Imune
-    self.V0 = V0
     self.Time = Time
     self.Goal = Schedule[Time[0]][Time[1]]
+    self.Age = Age
+    self.Prob_Die = 0.0000457*np.exp(0.08952*self.Age)
+    self.color =  {0:"Green",2:"Red",3:"Blue"}[self.Infect]
+    self.Death_period = Death_period
+    self.Incub_period = Incub_period
+    self.Recov_period = Recov_period
+    self.Infectivity = Infectivity
+    self.Is_Going_2_Die = False
+    self.timer = ["Mon",8,0]
+    self.Going_2_Quaren = False
 
 
 
-  def Set_Goal(self,time):
-    self.Goal = self.Schedule[time[0]][time[1]]
 
 
-  def Set_P0(self, PosiInicial):
-    self.Position = PosiInitial
-  
-  def Set_V0(self, v0):
-    self.Velocity = v0
+  def Set_Goal(self):
+    self.Goal = self.Schedule[self.Time[0]][self.Time[1]]
 
-  def att_posi(self):
+
+  def Att_Posi(self):
     self.Position = self.Position + self.Velocity*dt
-  
-  def att_velo(self):
-    self.Velocity = self.Velocity +a*dt
+
+  def Att_Time (self, Time):
+    self.Time = Time
+    self.Set_Goal()
+
+  def Att_State (self, *args):
+    if not self.Infect == -2:
+      if len(args) > 0:
+        self.timer = self.Time
+        self.Infect = args[0]
+        self.color =  {0:"Green",2:"Red",3:"Blue",1:"purple", -1:"Gray", -2:"Black"}[self.Infect]
+      else:
+        if ((self.Time[2] - self.timer[2]) >= self.Death_period) and (self.Is_Going_2_Die == True):
+          self.Infect = -2
+          self.color =  "Black"
+
+        elif (self.Time[2] - self.timer[2]) >= self.Incub_period and self.Infect == 1:
+
+          if np.random.uniform(0,1) <= self.Prob_Die:
+            self.Is_Going_2_Die = True
+
+          if np.random.uniform(0,1) <= asym:
+            self.Infect = 3
+            self.color =  "Blue"
+
+
+          else:
+            self.Infect = 2
+            self.color =  "Red"
+            self.Infectivity = np.random.gamma(1.3, 0.2)
+            while self.Infectivity > 1:
+                  self.Infectivity = np.random.gamma(1.3, 0.2)
+
+          self.timer = self.Time
+
+        elif (self.Time[2] - self.timer[2]) >= self.Recov_period and self.Infect >= 2:
+          self.Infect = -1
+          self.color = "Grey"
+          self.Is_Going_2_Die = False
+
+      if self.Quaren == True and self.Infect < 0:
+        self.Quaren = False
+      elif self.Quaren == False and np.random.uniform(0,1) <= 0.98:
+        if self.Infect == 2 and np.random.uniform(0,1) <= p_test_symp:
+          self.Going_2_Quaren = True
+        elif self.Infect == 3 and np.random.uniform(0,1) <= p_test_asymp:
+          self.Going_2_Quaren = True
+
+  def Att_Quarentine(self):
+    if self.Infect == -1:
+      self.Quaren = False
+      self.Going_2_Quaren == False
+      x = np.random.randint(-15,15)
+      y = np.random.randint(-15,15)
+      self.Position = np.array([x,y])
+
+    elif self.Going_2_Quaren == True:
+      self.Quaren =True
+      self.Position = np.array([100,100])           # Still need to think where quarentine will ocour.
--- Functions.py
+++ Functions.py
@@ -5,16 +5,16 @@ import random
 import matplotlib.animation as animation
 
 def Generate_Schedule(inst) -> dict:
-  # Create a schedule based on the institute that you specified 
+  # Create a schedule based on the institute that you specified
   # Note that the inst_list needs to have the institutes listed in the same order as the institutes in the p_list
 
 
   inst_list = ["IFGW","IC", "IMECC"]
   if inst == "IFGW":                                        # These are the probabilities of somoene who is in IFGW taking a class in each istitute
-    p_IFGW = 0.7                                            # For now, these are done manualy. We may need to find another way of doing it 
-    p_IC = 0.1                                               
+    p_IFGW = 0.7                                            # For now, these are done manualy. We may need to find another way of doing it
+    p_IC = 0.1
     p_IMECC = 0.2
-    
+
   if inst == "IMECC":
     p_IFGW = 0.05
     p_IC = 0.25
@@ -28,7 +28,7 @@ def Generate_Schedule(inst) -> dict:
   p_list =  [p_IFGW,p_IC,p_IMECC]
   d = ["Mon", "Tue", "Wed","Thu","Fri"]
   schedule = {}
-  
+
   for i in range(5):
     schedule[d[i]] = {}
     t = [8,9,10,11,14,15,16,17,18,19,20]                      # Times in wich you may have classes
@@ -41,85 +41,89 @@ def Generate_Schedule(inst) -> dict:
       schedule[d[i]][t[j]] = ""
     schedule[d[i]][12] = ""
     schedule[d[i]][13] = ""
-    
+
   return schedule
 
 
 def Create_Population(n,inst_distrib, vaci_prob, infect_prob, symp_prob, imune_prob) -> list:
-  # Creates a population for the University 
+  # Creates a population for the University
   # n is the number of people you want to create
-  # inst_distrib is the percentage of people in each institute: Needs to be a dictionary e.g.: {"IC": 0.25, "IFGW": 0.25, "IMECC": 0.5} 
-  # vaci_prob is the probability of someone beeing vacinated 
+  # inst_distrib is the percentage of people in each institute: Needs to be a dictionary e.g.: {"IC": 0.25, "IFGW": 0.25, "IMECC": 0.5}
+  # vaci_prob is the probability of someone beeing vacinated
   # infect_prob is the probaility of someone beeing infected in the beginning of the simulation
-  # symp_prob is the probability of someone infected showing symptoms 
-  # imune _prob is the probability of someone beeing imune in the beginning of the simulation 
+  # symp_prob is the probability of someone infected showing symptoms
+  # imune _prob is the probability of someone beeing imune in the beginning of the simulation
 
 
-  pop = []
-  inst_list = list(inst_distrib.keys())
-  inst_p = []
-  n_inst = len(inst_list)
-  buff = 0
-  vac_efi = 97/100
+    pop = []
+    inst_list = list(inst_distrib.keys())
+    inst_p = []
+    n_inst = len(inst_list)
+    buff = 0
+    vac_efi = 0
 
-  for i in inst_list:
-    buff += inst_distrib[i]
-    inst_p.append(buff)
+    for i in inst_list:
+        buff += inst_distrib[i]
+        inst_p.append(buff)
 
 
-  for i in range(n):
-    p_inst = random.randint(1,100)/100
-    p_infect = random.randint(1,100)/100
-    p_vaci =  random.randint(1,100)/100
-    p_vaci_efi = random.randint(1,100)/100
-    p_symp =  random.randint(1,100)/100
-    p_imune =  random.randint(1,100)/100
+    for i in range(n):
+        p_inst = random.randint(1,100)/100
+        p_infect = random.randint(1,100)/100
+        p_vaci =  random.randint(1,100)/100
+        p_vaci_efi = random.randint(1,100)/100
+        p_symp =  random.randint(1,100)/100
+        p_imune =  random.randint(1,100)/100
 
-    index = 0
-    for j in inst_p:
-      if p_inst <= inst_p[index]:
-        inst = inst_list[index]
         index = 0
-        break
-      index += 1
+        for j in inst_p:
+            if p_inst <= inst_p[index]:
+                inst = inst_list[index]
+                index = 0
+                break
+            index += 1
 
-    imune = False
-    if p_imune <= imune_prob:
-      imune = True 
+        imune = False
+        if p_imune <= imune_prob:
+            imune = True
 
-    vaci = False
-    if p_vaci <= vaci_prob and imune == False:
-      vaci = True
-      if p_vaci_efi <= vac_efi:
-        imune = True
+        vaci = False
+        if p_vaci <= vaci_prob and imune == False:
+            vaci = True
+        if p_vaci_efi <= vac_efi:
+            imune = True
 
-    infect = 0
-    if p_infect <= infect_prob and imune == False:
-      if p_symp <= symp_prob:
-        infect = 2
-      else:
-        infect = 3                  # Infected and symptomatic: infect = 2, Infected and Assymptomatic: infect = 3, Exposed: infect = 1, Susceptible: infect = 0, Recovered: infect = -1, Dead: infect = -2
+        infect = 0
+        if p_infect <= infect_prob and imune == False:
+            if p_symp <= symp_prob:
+                infect = 2
+            else:
+                infect = 3                  # Infected and symptomatic: infect = 2, Infected and Assymptomatic: infect = 3, Exposed: infect = 1, Susceptible: infect = 0, Recovered: infect = -1, Dead: infect = -2
 
 
-    Incub_period = np.round(np.random.lognormal(1.5, 0.6, n)/(8/24))
-    Death_period = np.round(np.random.lognormal(2.84, 0.58, n)/(8/24))       #8/24 is the dt in Pedro's code, don't know what it is going to be in this simulation, so I (Gustavo) left it unchanged.
-    Recov_period = np.round(np.random.gamma(2.2, 6.36, n)/(8/24))
-    
-    schedule = Generate_Schedule(inst)
-    pop.append(People(inst,infect,np.array([0,0]),vaci,np.array([0,0]),False, schedule , imune,np.array([0,0]),["Mon",8,1]),0,Incub_period ,Death_period,Recov_period)
+        incub_period = np.round(np.random.lognormal(1.5, 0.6, 1))[0]
+        death_period = np.round(np.random.lognormal(2.84, 0.58,1))[0]
+        recov_period = np.round(np.random.gamma(2.2, 6.36, 1))[0]
+        infectivity = np.random.gamma(1, 0.2, 1)[0]
+        while infectivity > 1:
+                            infectivity = np.random.gamma(1, 0.2, 1)[0]
 
-  return pop
+        schedule = Generate_Schedule(inst)
+        x = np.random.randint(-15,15)
+        y = np.random.randint(-15,15)
+        pop.append(People(inst,infect,np.array([x,y]),vaci,np.array([0,0]),False, schedule , imune,["Mon",8,1],0,incub_period,death_period,recov_period,infectivity))
+
+    return pop
 
 def Generate_University(Institute_list,fig,ax) -> None:
   for i in Institute_list:
     r = np.sqrt(i.area/np.pi)
     area = plt.Circle(i.location,r,fc = "lightblue", zorder = 0)
-    ax.scatter(i.location[0],i.location[1], color = "blue",zorder = 10)
     ax.add_patch(area)
 
 def random_walk (V) -> np.ndarray:
   theta  = np.random.choice(360)*np.pi/180
-  v = np.array([1,1])/(np.linalg.norm([1,1])) * V             #V = "Size" of the velocity vector 
+  v = np.array([1,1])/(np.linalg.norm([1,1])) * V             #V = "Size" of the velocity vector
   M = np.array([[np.cos(theta), -np.sin(theta)],[np.sin(theta),np.cos(theta)]])
   v = M.dot(v)
   return v
@@ -134,8 +138,17 @@ def detect_collision (p1, p2,R) -> bool:
   if (p1.Position[1] - p2.Position[1])**2 + (p1.Position[0] - p2.Position[0])**2 <= R**2:
     return True
 
-def solve_collision(p1,p2):
-  pass
+def solve_collision(p1,p2) -> None:
+    sig = 1.9/3
+    d = (p1.Position[0]-p2.Position[0])**2 + (p1.Position[1]-p2.Position[1])**2
+    prob = abs(-np.exp(-(1/2) * d/sig) - np.random.uniform(0,1))
+    if prob <= p1.Infectivity:
+        p2.Infectivity = np.random.gamma(0.65, 0.2)
+        while p2.Infectivity > 1:
+              p2.Infectivity = np.random.gamma(0.65, 0.2)
+        print("Um "+ str(p1.Infect) + " expos um " + str(p2.Infect))
+        p2.Att_State(1)
+
 
 def Sweep_n_prune(People,R) -> None:
   """
@@ -144,34 +157,37 @@ def Sweep_n_prune(People,R) -> None:
   #Sort people by the x-axis
   People = sorted(People, key=lambda People: People.Position[0])
   active = []
-  collision_set = {}
+  collision_set = set()
   for i in People:
-    
-    if len(active)>1:
-      #If there is at least one person in the active list and the interval of all the list coincides
-      if abs(active[0].Position[0] - i.Position[0]) <= R:
-        active.append(i)
-      # If the new person does not bellong to the currente interval we check all the collisions in the active list
-      else:
-        for j in active:
-          for k in active:
-            if detect_collision(j, k, R):
-              collision_set.add((j,k))
-
-        # We then remove the first item of the active list, since all of his possible collsions have been checked  
-        active.remove(active[0])
-
-        # We now start to remove all the itens of the active list, until the new item is in the interval of someone inside the active list or the active list is empty
-        for j in active:
-          if abs(j.Position[0] - i.Position[0]) <= R or len(active) == 0:
+    if i.Quaren == False:
+        if len(active)>1:
+          #If there is at least one person in the active list and the interval of all the list coincides
+          if abs(active[0].Position[0] - i.Position[0]) <= R:
             active.append(i)
-            break
-
+          # If the new person does not bellong to the currente interval we check all the collisions in the active list
           else:
-            active.remove(j)
+            for j in active:
+              for k in active:
+                if (j.Infect >= 1 or k.Infect >=1) and not (j.Infect >= 1 and k.Infect >=1 ) and not(j.Infect <0 or k.Infect <0 ) and detect_collision(j, k, R):
+                    if j.Infect>=1:
+                        collision_set.add((j,k))
+                    else:
+                        collision_set.add((k,j))
+
+            # We then remove the first item of the active list, since all of his possible collsions have been checked
+            active.remove(active[0])
+
+            # We now start to remove all the itens of the active list, until the new item is in the interval of someone inside the active list or the active list is empty
+            for j in active:
+              if abs(j.Position[0] - i.Position[0]) <= R or len(active) == 0:
+                active.append(i)
+                break
+
+              else:
+                active.remove(j)
+        else:
+            active.append(i)
 
   # We can now solve all the collisions
   for i in collision_set:
-    solve_colission (i[0],i[1])
-      
-
+    solve_collision(i[0],i[1])
